9dd3bfae6b1efe6e92f3def85d74ccd7fd5794a3
Hudi233 <1183460317@qq.com>
2019-05-09 15:26:38 +0800

    Update chaxun.py

--- iplist/chaxun.py
+++ iplist/chaxun.py
@@ -7,7 +7,7 @@ from ips.models import IPlist
 
 #print IPlist.objects.all()
 
-test1=IPlist.objects.get(ip_text='114.113.67.42',port_text='443')
+test1=IPlist.objects.get(ip_text='xxx',port_text='443')
 print test1
 
 #ips1 = Entry.objects.filter(ip_text="114.113.67.42",port_text="443")
