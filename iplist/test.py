51430adc35773a40f393ce71282d33e28c6cca37
Hudi233 <1183460317@qq.com>
2019-05-09 15:39:47 +0800

    Update test.py

--- iplist/test.py
+++ iplist/test.py
@@ -1,5 +1,5 @@
 import requests
 import json
-post_data = json.dumps({"ip":"100.73.49.130","memberID":"717598943303818513","uri":"/mybankv21/phptradeui/credit/indexToast"})
-info = requests.post("http://100.109.70.44:8000/threatinfo/search/", data = post_data, headers={'Content-Type': 'application/json'})
+post_data = json.dumps({"ip":"xxx","memberID":"xxx","uri":"xxx"})
+info = requests.post("http://xxx", data = post_data, headers={'Content-Type': 'application/json'})
 print(info.text)
